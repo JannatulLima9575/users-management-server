0a4261a21ea4887604e88bfe5e9e43855928d8d5
Jannatul <limajannat1675@gmail.com>
2025-05-15 21:35:40 +0600

    updates data

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('users server is running');
 })
+
+const users = [
+    { id: 1, name: 'Jannatul Lima', email: 'jannatullima@gmail.com'},
+    { id: 2, name: 'Sahmiha Rahman Ruba', email: 'rahmanruba@gmail.com'},
+    { id: 3, name: 'Tamanna Akter', email: 'aktertamanna@gmail.com'},
+]
+
+// for get data
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+// for post data
 
 app.listen(port, () => {
     console.log(`Example app listing port ${port}`);
